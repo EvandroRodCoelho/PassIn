ee87a83780d485b66fbbfeb03cb96a5d931a7561
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2024-04-03 09:28:16 -0300

    Adicionar arquivos de projeto.

---
+++ PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassIn.Application.UseCases.Events.GetById
+{
+    public class GetEventByIdUseCase
+    {
+        public  ResponseEventJson Execute(Guid id)
+        {
+            var dbContext = new PassInDbContext();
+
+            var entity = dbContext.Events.FirstOrDefault(e => e.Id == id);
+
+            if (entity is null)
+                throw new PassInException("An event with this id dont exists.");
+
+            return new ResponseEventJson
+            {
+                Id = entity.Id,
+                Title = entity.Title,
+                Details = entity.Details,
+                MaximumAttendees = entity.Maximum_Attendees,
+                AttendeesAmount = -1
+            };
+
+        }
+    }
+}
